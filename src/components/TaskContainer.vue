c042a0d2aa33aa8ce9ce83de49d16d3ad2b087f4
Piotr Wójcik <krakownh1@gmail.com>
2019-12-29 19:31:08 +0100

    Repair errors 2

--- src/components/TaskContainer.vue
+++ src/components/TaskContainer.vue
@@ -1,26 +1,48 @@
 <template>
-    <div class="bg-gray-100  mb-2 flex bordered rounded-l-lg h-32 " :class="{'bg-red-500' : taskData.prio, 'bg-green-600' : !taskData.prio}">
-        <div class="flex items-center align-center mx-2 border-l-xs"><i :class="taskData.icon" class="text-gray-300"></i></div>
-                
-        <div class="bg-white px-2 py-1 flex-grow relative">
-            <p class="font-bold">{{taskData.title}}</p>
-            <p>{{taskData.description}}</p>
-            <p class="text-sm">Dodano: {{taskData.addDate + " " + taskData.addTime}}</p>
-            <!-- {{taskData}} -->
-            <div class="bg-orange-500 text-center flex timer items-center pl-2" v-if="showTimer">
-            <i class="fas fa-stopwatch text-white mr-2"></i> 
-            <p v-if="!endOfTime" class="text-white">{{ days }} dni {{ hours }} : {{minutes}} : {{seconds}}</p> 
-            <p v-else class="text-white">KONIEC CZASU!</p>
-            
-            </div>
-        </div>
-        <div class="flex flex-col align-between justify-around pr-2 bg-gray-200 shadow-lg text-2xl px-2">
-            <button @click="deleteTask(taskData.id)"><i class="fas fa-times text-red-700"></i></button>
-            <button @click="() => this.inEdit = !this.inEdit"><i class="fas fa-edit text-blue-700"></i></button>
-            <button><i class="far fa-check-circle text-green-700"></i></button>
-        </div>
-        <EditTask :taskData="taskData" :dateActive="date" v-if="inEdit" v-on:cancel="() => this.inEdit = !this.inEdit" />
-            </div>
+  <div
+    class="bg-gray-100  mb-2 flex bordered rounded-l-lg h-32 "
+    :class="{ 'bg-red-500': taskData.prio, 'bg-green-600': !taskData.prio }"
+  >
+    <div class="flex items-center align-center mx-2 border-l-xs">
+      <i :class="taskData.icon" class="text-gray-300"></i>
+    </div>
+
+    <div class="bg-white px-2 py-1 flex-grow relative">
+      <p class="font-bold">{{ taskData.title }}</p>
+      <p>{{ taskData.description }}</p>
+      <p class="text-sm">
+        Dodano: {{ taskData.addDate + " " + taskData.addTime }}
+      </p>
+      <!-- {{taskData}} -->
+      <div
+        class="bg-orange-500 text-center flex timer items-center pl-2"
+        v-if="showTimer"
+      >
+        <i class="fas fa-stopwatch text-white mr-2"></i>
+        <p v-if="!endOfTime" class="text-white">
+          {{ days }} dni {{ hours }} : {{ minutes }} : {{ seconds }}
+        </p>
+        <p v-else class="text-white">KONIEC CZASU!</p>
+      </div>
+    </div>
+    <div
+      class="flex flex-col align-between justify-around pr-2 bg-gray-200 shadow-lg text-2xl px-2"
+    >
+      <button @click="deleteTask(taskData.id)">
+        <i class="fas fa-times text-red-700"></i>
+      </button>
+      <button @click="() => (this.inEdit = !this.inEdit)">
+        <i class="fas fa-edit text-blue-700"></i>
+      </button>
+      <button><i class="far fa-check-circle text-green-700"></i></button>
+    </div>
+    <EditTask
+      :taskData="taskData"
+      :dateActive="date"
+      v-if="inEdit"
+      v-on:cancel="() => (this.inEdit = !this.inEdit)"
+    />
+  </div>
 </template>
 
 <script>
@@ -97,8 +119,6 @@ export default {
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
-
-        console.log(days + " " + hours + " " + minutes + " " + seconds);
       }, 1000);
     }
   },
